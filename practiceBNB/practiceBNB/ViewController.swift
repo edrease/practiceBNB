8a1df2b94a5c4668d4d94c2e07e46dbb1118c1f4
Mick Soum <mick26soum@gmail.com>
2015-08-21 14:39:05 -0700

    Second Commit From Mick

--- practiceBNB/practiceBNB/ViewController.swift
+++ practiceBNB/practiceBNB/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 		println("hi")
+		println("Another Print")
     // Do any additional setup after loading the view, typically from a nib.
   }
 
