fe181fc18f4a34a118553a654f58734524c1dac4
Edrease Peshtaz <edrease@gmail.com>
2015-08-21 14:28:53 -0700

    Added a class for maps.

---
+++ practiceBNB/practiceBNB/MapsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MapsViewController.swift
+//  practiceBNB
+//
+//  Created by Edrease Peshtaz on 8/21/15.
+//  Copyright (c) 2015 cf. All rights reserved.
+//
+
+import UIKit
+
+class MapsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        println("practiceBNB sounds really neat")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
