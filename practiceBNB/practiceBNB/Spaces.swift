2d09ed0a38eb91e000e2d9b32a59c3ecbabac0a5
mcclure15 <m001010111@gmail.com>
2015-08-21 14:57:21 -0700

    add some files

---
+++ practiceBNB/practiceBNB/Spaces.swift
@@ -0,0 +1,14 @@
+//
+//  Spaces.swift
+//  practiceBNB
+//
+//  Created by Matthew McClure on 8/21/15.
+//  Copyright (c) 2015 cf. All rights reserved.
+//
+
+import Foundation
+
+struct Spaces {
+  let space : String
+  var price : Double 
+}
